4d37212976827e0aafe5bcd532b1939761ea4a55
Nayan Raghuvir <raghuvir.nayan@gmail.com>
2022-12-02 08:07:13 +0530

    Array HW-2

--- SkillMineCodes/Array/HW2.cs
+++ SkillMineCodes/Array/HW2.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SkillMineCodes.Nested_Loop;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,6 +8,31 @@ using System.Xml.Linq;
 
 namespace SkillMineCodes.Array
 {
+
+    /*Q1.Display alternate no element*/
+    class Q1
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(System.Console.ReadLine());
+
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                a[i] = int.Parse(Console.ReadLine());
+            }
+            Q1 o = new Q1();
+            o.fun(a);
+        }
+
+        public void fun(int[] a)
+        {
+            for(int i=0; i<a.Length; i += 2)
+            {
+                Console.Write(a[i]+" ");
+            }
+        }
+    }
 
     /*Q4.Frequency of given element.*/
     class Q4
