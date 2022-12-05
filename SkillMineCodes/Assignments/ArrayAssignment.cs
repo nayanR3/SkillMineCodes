d6956bb38a3dd8bacd7547982d5871efd39d0c2f
Nayan Raghuvir <raghuvir.nayan@gmail.com>
2022-12-05 17:11:14 +0530

    Array Assignment

--- SkillMineCodes/Assignments/ArrayAssignment.cs
+++ SkillMineCodes/Assignments/ArrayAssignment.cs
@@ -1,12 +1,6 @@
-﻿using SkillMineCodes.AccessModifiers;
-using System;
+﻿using System;
 using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.NetworkInformation;
-using System.Runtime.Remoting.Messaging;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace SkillMineCodes.Assignments
 {
@@ -582,5 +576,272 @@ have elements of same value.*/
             Console.WriteLine();
         }
 
+    }
+
+
+
+    /*Q11.WAP sort array elements in ascending order.*/
+    class ArrayQ11
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter array size");
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = new int[n];
+            System.Console.WriteLine();
+            System.Console.WriteLine("Enter Array element :");
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+            System.Console.WriteLine();
+            System.Console.WriteLine("Entered Array is :");
+            System.Console.WriteLine(string.Join(" ", arr));
+
+            ArrayQ11 p = new ArrayQ11();
+            p.fun(arr);
+        }
+
+        public void fun(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = 0; j < arr.Length - i - 1; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        int t = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = t;
+                    }
+                }
+            }
+            System.Console.WriteLine();
+            System.Console.WriteLine("Sorted Array");
+            System.Console.WriteLine(string.Join(" ", arr));
+        }
+    }
+
+
+
+
+    /* Q12. WAP to reverse the array itself, 
+            don’t print array in reverse
+            I want current array reverse.
+            Means e.g.arr[] = [3, 90, 45,29, 37, 78] 
+            so your same array must be [78, 37, 29, 45, 90,3] 
+            without using temporary array */
+    class ArrayQ12
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter array size");
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = new int[n];
+            System.Console.WriteLine();
+            System.Console.WriteLine("Enter Array element :");
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+            System.Console.WriteLine();
+            System.Console.WriteLine("Entered Array is :");
+            System.Console.WriteLine(string.Join(" ", arr));
+
+            ArrayQ12 p = new ArrayQ12();
+            p.fun(arr);
+        }
+
+        public void fun(int[] a)
+        {
+            int n = a.Length;
+            for (int i = 0; i < n / 2; i++)
+            {
+                int t = a[i];
+                a[i] = a[n - i - 1];
+                a[n - i - 1] = t;
+            }
+            System.Console.WriteLine();
+            System.Console.WriteLine("Reverse Array");
+            System.Console.WriteLine(string.Join(" ", a));
+        }
+    }
+
+
+
+    /*Q13.Write a program to test the equality of two arrays.
+          Means e.g arr1[] = [12, 22, 32, 42, 52,62] and 
+                    arr2[] = [52, 22, 62, 12, 42, 22] 
+          Here both arrays are equal.*/
+    class ArrayQ13
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Size of array");
+            int n = int.Parse(Console.ReadLine());
+            int[] a1 = new int[n];
+            int[] a2 = new int[n];
+
+            System.Console.WriteLine();
+            Console.WriteLine("Enter 1st Array element");
+            for (int i = 0; i < n; i++)
+            {
+                a1[i] = int.Parse(Console.ReadLine());
+            }
+            System.Console.WriteLine();
+            Console.WriteLine("Enter 2nd Array element");
+            for (int i = 0; i < n; i++)
+            {
+                a2[i] = int.Parse(Console.ReadLine());
+            }
+            ArrayQ13 p = new ArrayQ13();
+            p.fun(a1, a2);
+        }
+
+        public void fun(int[] a1, int[] a2)
+        {
+            // sorting 1st array
+            for (int i = 0; i < a1.Length - 1; i++)
+            {
+                for (int j = 0; j < a1.Length - i - 1; j++)
+                {
+                    if (a1[j] > a1[j + 1])
+                    {
+                        int t = a1[j];
+                        a1[j] = a1[j + 1];
+                        a1[j + 1] = t;
+                    }
+                }
+            }
+            System.Console.WriteLine();
+            Console.WriteLine("Sorted 1st Array");
+            Console.WriteLine(string.Join(" ", a1));
+
+            // sorting 2nd array
+            for (int i = 0; i < a2.Length - 1; i++)
+            {
+                for (int j = 0; j < a2.Length - i - 1; j++)
+                {
+                    if (a2[j] > a2[j + 1])
+                    {
+                        int t = a2[j];
+                        a2[j] = a2[j + 1];
+                        a2[j + 1] = t;
+                    }
+                }
+            }
+            System.Console.WriteLine();
+            Console.WriteLine("Sorted 2nd Array");
+            Console.WriteLine(string.Join(" ", a2));
+
+            System.Console.WriteLine();
+            bool f = true;
+            for (int i = 0; i < a1.Length; i++)
+            {
+                if (a1[i] != a2[i])
+                {
+                    f = false;
+                    break;
+                }
+            }
+
+            if (f == true)
+            {
+                System.Console.WriteLine("Matched Array");
+            }
+            else
+            {
+                System.Console.WriteLine("Unmatched");
+            }
+        }
+    }
+
+
+
+    /*Q14.WAP to replace all negative value with its immediate left elements square.
+    Means arr[] = [12, 3, -19, 29, 5, -61, 44, 7, -9] 
+    Output array will be [12, 3, 9, 29, 5, 25, 44, 7, 49].*/ 
+    class ArrayQ14
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Size of array");
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = new int[n];
+
+            System.Console.WriteLine();
+            Console.WriteLine("Enter  Array element");
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+
+            ArrayQ14 p = new ArrayQ14();
+            p.fun(arr);
+        }
+
+        public void fun(int[] arr)
+        {
+            System.Console.WriteLine();
+            int i = 0;
+            while (i < arr.Length)
+            {
+                int v = arr[i];
+                if (v < 0)
+                {
+                    arr[i] = arr[i - 1] * arr[i - 1];
+                }
+
+                i++;
+            }
+            System.Console.WriteLine(string.Join(" ", arr));
+        }
+    }
+
+    
+
+
+    /*Q15.WAP to arrange the elements of an given array of integers 
+        where all negative integers appear before all the positive integers.*/
+    class ArrayQ15
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Size of array");
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = new int[n];
+
+            System.Console.WriteLine();
+            Console.WriteLine("Enter  Array element");
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+            System.Console.WriteLine();
+            System.Console.WriteLine("Entered Array is");
+            System.Console.WriteLine(string.Join(" ", arr));
+
+            ArrayQ15 p = new ArrayQ15();
+            p.fun(arr);
+        }
+
+        public void fun(int[] arr)
+        {
+            System.Console.WriteLine();
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                for (int j = 0; j < arr.Length - 1 - i; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        int t = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = t;
+                    }
+                }
+            }
+            System.Console.WriteLine("Negative Positive array");
+            System.Console.WriteLine(string.Join(" ", arr));
+        }
     }
 }
