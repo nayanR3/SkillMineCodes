0455c6d2eb259196e7fb52f498b7f01ba06e66ba
Nayan Raghuvir <raghuvir.nayan@gmail.com>
2022-12-11 08:33:53 +0530

    Date 10-Dec-2022 Written test Quesions

--- SkillMineCodes/WrittenTest/Dec10/Questions.cs
+++ SkillMineCodes/WrittenTest/Dec10/Questions.cs
@@ -1,18 +1,56 @@
-﻿using System;
+﻿using SkillMineCodes.AccessModifiers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SkillMineCodes.WrittenTest.Dec10
 {
+    /*Q1. Check if given number is Armstrong or not*/
+    public class DecQ1
+    {
+        private int n;
+
+        public DecQ1(int n)
+        {
+            this.n = n;
+        }
+
+        public string Cal()
+        {
+            int copy = n;
+            int sum = 0;
+
+            while (copy != 0)
+            {
+                int rem = copy % 10;
+                copy /= 10;
+                sum += (rem * rem * rem);
+            }
+
+            string s = "";
+            if (sum == n)
+            {
+                s += $"{n} is Armstrong number";
+            }
+            else
+            {
+                s += $"{n} is not a Armstrong number";
+            }
+            return s;
+        }
+    }
+
+
     /*Q2. Swap values of two integer variables without using third variable.*/
     public class DecQ2
     {
         public int A { get; set; }
         public int B { get; set; }
 
-        public void Cal( )
+        public void Cal()
         {
             A = A + B;
             B = A - B;
@@ -24,4 +62,73 @@ namespace SkillMineCodes.WrittenTest.Dec10
         }
 
     }
+
+
+    /*Q3. Count number of digits in any number.*/
+    public class DecQ3
+    {
+        private int n;
+        public DecQ3(int n)
+        {
+            this.n = n;
+        }
+
+
+        int d = 0;
+        public void CountDigit()
+        {
+            int copy = n;
+            while (copy != 0)
+            {
+                int r = copy % 10;
+                d++;
+                copy /= 10;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Total digits in number {n} is {d}";
+        }
+    }
+
+
+    /* 4. Accept a number from user and Create table of a number.*/
+    public class DecQ4
+    {
+        private int n;
+        public DecQ4(int n)
+        {
+            this.n = n;
+        }
+
+        public void Table()
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"{n} * {i} = {n * i}");
+            }
+        }
+    }
+
+
+    /*Q5. Find first 10 even numbers.*/
+    public class DecQ5
+    {
+        public void Even()
+        {
+            int c = 0;
+            int v = 1;
+            while (c != 10)
+            {
+                if (v % 2 == 0)
+                {
+                    Console.WriteLine(v);
+                    c++;
+                }
+                v++;
+            }
+        }
+    }
+
 }
