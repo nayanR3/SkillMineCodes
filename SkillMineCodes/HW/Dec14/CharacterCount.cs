66aa14dcbb59d34fab6643ac4af7b3a2d7cfd9c3
Nayan Raghuvir <raghuvir.nayan@gmail.com>
2022-12-15 10:23:59 +0530

    Homework

---
+++ SkillMineCodes/HW/Dec14/CharacterCount.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkillMineCodes.HW.Dec14
+{
+    public class CharacterCount
+    {
+        public static void Main(string[] args)
+        {
+            String s = "nayanRnn";
+            char[] chArr = s.ToCharArray();
+            Console.WriteLine("Enter Character");
+            char ch = Convert.ToChar(Console.ReadLine());
+            fun(chArr, ch);
+        }
+
+        public static void fun(char[] chArr, char ch)
+        {
+            int c = 0;
+            for (int i = 0; i < chArr.Length; i++)
+            {
+                if (chArr[i] == ch)
+                {
+                    c++;
+                }
+            }
+            Console.WriteLine($"Count of {ch} char is {c}");
+        }
+    }
+}
---
+++ SkillMineCodes/HW/Dec14/Palindrome.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkillMineCodes.HW.Dec14
+{
+    public class Palindrome
+    {
+        public static void Main(string[] args)
+        {
+            String s = "madam";
+            char[] ch = s.ToCharArray();
+            fun(ch);
+        }
+
+        public static void fun(char[] ch)
+        {
+            int i = 0, j = ch.Length - 1;
+            bool flag = true;
+            while (i < j)
+            {
+                while (ch[i] != ch[j])
+                {
+                    flag = false;
+                    break;
+                }
+                i++;
+                j--;
+            }
+
+            if (flag == true)
+            {
+                Console.WriteLine("Palindrom");
+            }
+            else
+            {
+                Console.WriteLine("Not a Palindrom");
+            }
+        }
+    }
+}
