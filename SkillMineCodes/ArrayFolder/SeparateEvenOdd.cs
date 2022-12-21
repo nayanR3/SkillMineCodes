03dee914b173d764547d0f2b44ac149491a4a0c9
NAYAN <nayanraghuvir1@gmail.com>
2022-12-21 17:25:37 +0530

    Separate even odd elements from array

---
+++ SkillMineCodes/ArrayFolder/SeparateEvenOdd.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+
+namespace ArrayFolder
+{
+    public class SeparateEvenOdd
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(System.Console.ReadLine());
+
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                a[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine(string.Join(" ", a));
+            fun (a);
+        }
+
+        public static void fun(int[] a)
+        {
+            ArrayList odd = new ArrayList();
+            ArrayList even = new ArrayList();
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i] % 2 == 0)
+                {
+                    even.Add(a[i]);
+                }
+                else
+                {
+                    odd.Add(a[i]);
+                }
+            }
+
+            foreach (int i in odd)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+
+            foreach (int i in even)
+            {
+                Console.Write(i + " ");
+            }
+        }
+    }
+}
