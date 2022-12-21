1fc8ce5c1b4ce313028eb8e670d41808b8e6dd12
NAYAN <nayanraghuvir1@gmail.com>
2022-12-21 15:19:52 +0530

    readme file added

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # SkillMineCodes
+# List of Topic
+
+## 01 . [`OOP's`](https://github.com/nayanR3/POTD/blob/master/md%20files/01.%20SEP-2022.md)
+
+## 02 . [`Array`](https://github.com/nayanR3/POTD/blob/master/md%20files/02.%20OCT-2022.md)
+
+## 03 . [`String`](https://github.com/nayanR3/POTD/blob/master/md%20files/03.%20NOV-2022.md)
+
+## 04 . [`Exception Handling`](https://github.com/nayanR3/POTD/blob/master/md%20files/04.%20DEC-2022.md)
---
+++ SkillMineCodes/Bank.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SkillMineCodes
+{
+    public enum AccountType { Saving, Current }
+    public class Customer
+    {
+        public int AccNo { get; set; }
+        public string Name { get; set; }
+        public AccountType AccountType;
+        public List<Loan> Loans = new List<Loan>();
+    }
+    public class Loan
+    {
+        public int Id { get; set; }
+        public string LoanName { get; set; }
+    }
+    public class PP
+    {
+        static void Main(string[] args)
+        {
+            List<Customer> list = new List<Customer>()
+            {
+                new Customer
+                {
+                    AccNo = 101,
+                    Name="Customer1",
+                    AccountType=AccountType.Saving,
+                    Loans =
+                    {
+                        new Loan{Id=1,LoanName="Car"},
+                        new Loan{Id=2,LoanName="Home"},
+                    }
+                },
+                new Customer
+                {
+                    AccNo = 102,
+                     Name="Customer2",
+                    AccountType=AccountType.Current,
+                    Loans =
+                    {
+                        new Loan{Id=1,LoanName="Business"},
+
+                    }
+                }
+            };
+
+            foreach (Customer c in list)
+            {
+                Console.WriteLine($"{c.Name}");
+                Console.WriteLine($"\t{c.AccountType}");
+                foreach (Loan L in c.Loans)
+                {
+                    Console.WriteLine("\t\t" + L.LoanName);
+                }
+            }
+        }
+
+
+    }
+
+}
