4125ff4a65ece0ce18070011e9ac0a6286281d9d
NAYAN <nayanraghuvir1@gmail.com>
2022-12-22 07:01:46 +0530

    String palindrome

---
+++ SkillMineCodes/StringFolder/Palindrome.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace SkillMineCodes.StringFolder
+{
+    public class Palindrome
+    {
+        public static void Main(string[] args)
+        {
+            String s = "madam";
+            char[] ch = s.ToCharArray();
+            fun(ch);
+        }
+
+        public static void fun(char[] ch)
+        {
+            int i = 0, j = ch.Length - 1;
+            bool flag = true;
+            while (i < j)
+            {
+                while (ch[i] != ch[j])
+                {
+                    flag = false;
+                    break;
+                }
+                i++;
+                j--;
+            }
+
+            if (flag == true)
+            {
+                Console.WriteLine("Palindrom");
+            }
+            else
+            {
+                Console.WriteLine("Not a Palindrom");
+            }
+        }
+    }
+}
