d62ca04d63cab6ab9ae185bb82956e33aa268ce0
NAYAN <nayanraghuvir1@gmail.com>
2022-12-22 07:01:24 +0530

    Toggle the letters of string & display

---
+++ SkillMineCodes/StringFolder/CharToggle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace SkillMineCodes.StringFolder
+{
+    public class CharToggle
+    {
+        public static void Main(String[] args)
+        {
+            String s = "hello WORLD";
+            char[] arr = s.ToCharArray();
+            fun(arr);
+            Console.WriteLine("String after toggle ");
+            Console.WriteLine();
+            Console.WriteLine(String.Join("", arr));
+        }
+
+        static void fun(char[] str)
+        {
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] >= 'A' && str[i] <= 'Z')
+                    str[i] = (char)(str[i] + 'a' - 'A');
+                else if (str[i] >= 'a' && str[i] <= 'z')
+                    str[i] = (char)(str[i] + 'A' - 'a');
+            }
+        }
+    }
+}
