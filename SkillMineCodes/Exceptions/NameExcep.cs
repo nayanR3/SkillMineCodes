a1134a312dfae7a318a84017638fa750373be138
NAYAN <nayanraghuvir1@gmail.com>
2022-12-22 06:30:17 +0530

    Custom Exception : Name

---
+++ SkillMineCodes/Exceptions/NameExcep.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace SkillMineCodes.Exceptions
+{
+    public class Stud2
+    {
+        private string name;
+
+        public void AcceptName(string name)
+        {
+            if (name == null || name == "")
+            {
+                throw new NameException("Name can not be null or empty");
+            }
+            else
+            {
+                this.name = name;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{this.name}";
+        }
+    }
+
+    public class NameException : Exception
+    {
+        public NameException(string message) :
+            base(message)
+        {
+        }
+    }
+
+    public class Program2
+    {
+        static void Main(string[] args)
+        {
+            Stud2 s = new Stud2();
+            try
+            {
+                s.AcceptName(name: null);
+                System.Console.WriteLine (s);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                System.Console.WriteLine("Accept");
+            }
+        }
+    }
+}
