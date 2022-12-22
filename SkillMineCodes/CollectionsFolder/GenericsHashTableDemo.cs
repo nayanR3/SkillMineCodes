bb202e9eaabe985a7300d2871d6bb0aa44207840
NAYAN <nayanraghuvir1@gmail.com>
2022-12-22 07:44:59 +0530

    Generics HashTable Demo

---
+++ SkillMineCodes/CollectionsFolder/GenericsHashTableDemo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+
+namespace SkillMineCodes.CollectionsFolder
+{
+    public class GenericsHashTableDemo
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> d1 = new Dictionary<string, int>();
+            d1.Add("phy", 70);
+            d1.Add("chem", 77);
+            d1.Add("bio", 55);
+            d1.Add("maths", 60);
+
+            Console.WriteLine("dictionary");
+
+            foreach (KeyValuePair<string, int> i in d1)
+            {
+                Console.WriteLine(i.Key + " " + i.Value);
+            }
+        }
+    }
+}
