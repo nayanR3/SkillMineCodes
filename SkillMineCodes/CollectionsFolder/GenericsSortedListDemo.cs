392f00723e57b72b371f2c031e5056e88df72a05
NAYAN <nayanraghuvir1@gmail.com>
2022-12-22 07:45:49 +0530

    Generics SortedList Demo

---
+++ SkillMineCodes/CollectionsFolder/GenericsSortedListDemo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+
+namespace SkillMineCodes.CollectionsFolder
+{
+    public class GenericsSortedListDemo
+    {
+        static void Main(string[] args)
+        {
+            SortedList<int,int> sl = new SortedList<int,int>();
+
+            sl.Add(1, 11);
+            sl.Add(33, 2);
+            sl.Add(678, 9);
+            sl.Add(8, 345);
+            Console.WriteLine("\nSortedList element");
+            foreach (KeyValuePair<int,int> i in sl)
+            {
+                Console.Write($"key -> {i.Key} & value -> {i.Value}");
+                Console.WriteLine();
+            }
+        }
+    }
+}
