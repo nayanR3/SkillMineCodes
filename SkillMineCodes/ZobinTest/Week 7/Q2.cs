545483c7167c916b613fe15a90e0d6975bc9a7f2
NAYAN <nayanraghuvir1@gmail.com>
2023-01-16 19:32:24 +0530

    feat: create Q1.cs and Q2.cs

---
+++ SkillMineCodes/ZobinTest/Week 7/Q2.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SkillMineCodes.ZobinTest.Week_7
+{
+    public class Q2
+    {
+        
+    }
+}
