32d5d31ba35eeb040c2f35588c043ab23c429676
NAYAN <nayanraghuvir1@gmail.com>
2023-01-16 19:23:46 +0530

    update Program.cs and SkillMineCodes.csproj

--- SkillMineCodes/Program.cs
+++ SkillMineCodes/Program.cs
@@ -1,44 +1,14 @@
 ﻿using System;
 using System.Collections;
 using System.Text;
+using System.Xml.Schema;
 
 namespace SkillMineCodes
 {
     public class Program
     {
-        static void Main()
+        static void Main(string[] args)
         {
-            int[] arr = { 12, 4, 2, 10, 5, 1 };
-            int n = 16;
-
-            int sum = arr[0];
-            int start = 0;
-
-            for (int i = 1; i < arr.Length; i++)
-            {
-                sum = sum + arr[i];
-
-                while (sum > n && start <= i - 1)
-                {
-                    sum = sum - arr[start];
-
-                    start++;
-                }
-
-                if (sum == n)
-                {
-                    Console.WriteLine("Continuous sub array of whose sum is " + n);
-
-                    for (int j = start; j <= i; j++)
-                    {
-                        Console.WriteLine(arr[j] + " ");
-                    }
-                    Console.WriteLine();
-                }
-            }
-
         }
-
     }
-
 }
