e73121a039fbf4376bbf1fa4f7e9af8d2a248ecf
NAYAN <nayanraghuvir1@gmail.com>
2023-01-23 09:41:24 +0530

    .

--- SkillMineCodes/Program.cs
+++ SkillMineCodes/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Xml.Schema;
 
@@ -9,6 +11,38 @@ namespace SkillMineCodes
     {
         static void Main(string[] args)
         {
+            string s = "abcccc";
+            fnc(s);
+        }
+
+        public static void fnc(string s)
+        {
+            int n = s.Length;
+            int[] freq = new int[26];
+            int[] f2 = new int[26];
+
+            for (int i = 0; i < n; i++)
+            {
+                freq[s[i] - 'a']++;
+                f2[s[i] - 'a']++;
+            }
+
+            int sm = m(f2);
+
+            Console.WriteLine(sm);
+
+            for (int i = 0; i < freq.Length; i++)
+                if (freq[i] == sm)
+                    Console.WriteLine((char)(i + 'a') + " " + freq[i]);
+        }
+
+
+        public static int m(int[] freq)
+        {
+            
+            Array.Sort(freq);
+
+            return freq[25];
         }
     }
 }
