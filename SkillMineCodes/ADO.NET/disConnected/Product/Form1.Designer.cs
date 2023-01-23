e3cbf0f56c406e9f310cc6f6d78322a18adc9cd9
NAYAN <nayanraghuvir1@gmail.com>
2023-01-24 00:14:46 +0530

    product project ado.net dis-connected

---
+++ SkillMineCodes/ADO.NET/disConnected/Product/Form1.Designer.cs
@@ -0,0 +1,270 @@
+﻿namespace ProductDisconnect
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnSA = new System.Windows.Forms.Button();
+            this.GridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblCN = new System.Windows.Forms.Label();
+            this.lblPP = new System.Windows.Forms.Label();
+            this.lblPN = new System.Windows.Forms.Label();
+            this.lblPI = new System.Windows.Forms.Label();
+            this.txtCN = new System.Windows.Forms.TextBox();
+            this.txtPP = new System.Windows.Forms.TextBox();
+            this.txtPN = new System.Windows.Forms.TextBox();
+            this.txtPI = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.GridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(290, 435);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(101, 35);
+            this.btnClear.TabIndex = 32;
+            this.btnClear.Text = "CLEAR";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnSA
+            // 
+            this.btnSA.Location = new System.Drawing.Point(710, 340);
+            this.btnSA.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btnSA.Name = "btnSA";
+            this.btnSA.Size = new System.Drawing.Size(101, 37);
+            this.btnSA.TabIndex = 31;
+            this.btnSA.Text = "SHOW ALL";
+            this.btnSA.UseVisualStyleBackColor = true;
+            this.btnSA.Click += new System.EventHandler(this.btnSA_Click);
+            // 
+            // GridView1
+            // 
+            this.GridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.GridView1.Location = new System.Drawing.Point(556, 387);
+            this.GridView1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.GridView1.Name = "GridView1";
+            this.GridView1.Size = new System.Drawing.Size(420, 165);
+            this.GridView1.TabIndex = 30;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(366, 22);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(266, 50);
+            this.label1.TabIndex = 29;
+            this.label1.Text = "PRODUCT ";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblCN
+            // 
+            this.lblCN.AutoSize = true;
+            this.lblCN.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCN.Location = new System.Drawing.Point(337, 234);
+            this.lblCN.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblCN.Name = "lblCN";
+            this.lblCN.Size = new System.Drawing.Size(150, 22);
+            this.lblCN.TabIndex = 28;
+            this.lblCN.Text = "Company Name";
+            // 
+            // lblPP
+            // 
+            this.lblPP.AutoSize = true;
+            this.lblPP.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPP.Location = new System.Drawing.Point(338, 199);
+            this.lblPP.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblPP.Name = "lblPP";
+            this.lblPP.Size = new System.Drawing.Size(131, 22);
+            this.lblPP.TabIndex = 27;
+            this.lblPP.Text = "Product Price";
+            // 
+            // lblPN
+            // 
+            this.lblPN.AutoSize = true;
+            this.lblPN.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPN.Location = new System.Drawing.Point(338, 161);
+            this.lblPN.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblPN.Name = "lblPN";
+            this.lblPN.Size = new System.Drawing.Size(136, 22);
+            this.lblPN.TabIndex = 26;
+            this.lblPN.Text = "Product Name";
+            // 
+            // lblPI
+            // 
+            this.lblPI.AutoSize = true;
+            this.lblPI.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPI.Location = new System.Drawing.Point(338, 125);
+            this.lblPI.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblPI.Name = "lblPI";
+            this.lblPI.Size = new System.Drawing.Size(101, 22);
+            this.lblPI.TabIndex = 25;
+            this.lblPI.Text = "Product Id";
+            // 
+            // txtCN
+            // 
+            this.txtCN.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCN.Location = new System.Drawing.Point(567, 231);
+            this.txtCN.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.txtCN.Name = "txtCN";
+            this.txtCN.Size = new System.Drawing.Size(116, 27);
+            this.txtCN.TabIndex = 24;
+            // 
+            // txtPP
+            // 
+            this.txtPP.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPP.Location = new System.Drawing.Point(567, 194);
+            this.txtPP.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.txtPP.Name = "txtPP";
+            this.txtPP.Size = new System.Drawing.Size(116, 27);
+            this.txtPP.TabIndex = 23;
+            // 
+            // txtPN
+            // 
+            this.txtPN.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPN.Location = new System.Drawing.Point(567, 156);
+            this.txtPN.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.txtPN.Name = "txtPN";
+            this.txtPN.Size = new System.Drawing.Size(116, 27);
+            this.txtPN.TabIndex = 22;
+            // 
+            // txtPI
+            // 
+            this.txtPI.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPI.Location = new System.Drawing.Point(567, 122);
+            this.txtPI.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.txtPI.Name = "txtPI";
+            this.txtPI.Size = new System.Drawing.Size(116, 27);
+            this.txtPI.TabIndex = 21;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSearch.Location = new System.Drawing.Point(409, 387);
+            this.btnSearch.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(101, 38);
+            this.btnSearch.TabIndex = 20;
+            this.btnSearch.Text = "SEARCH";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelete.Location = new System.Drawing.Point(290, 387);
+            this.btnDelete.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(101, 38);
+            this.btnDelete.TabIndex = 19;
+            this.btnDelete.Text = "DELETE";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpdate.Location = new System.Drawing.Point(409, 340);
+            this.btnUpdate.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(101, 38);
+            this.btnUpdate.TabIndex = 18;
+            this.btnUpdate.Text = "UPDATE";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSave.Location = new System.Drawing.Point(290, 340);
+            this.btnSave.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(101, 38);
+            this.btnSave.TabIndex = 17;
+            this.btnSave.Text = "SAVE";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1003, 521);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnSA);
+            this.Controls.Add(this.GridView1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblCN);
+            this.Controls.Add(this.lblPP);
+            this.Controls.Add(this.lblPN);
+            this.Controls.Add(this.lblPI);
+            this.Controls.Add(this.txtCN);
+            this.Controls.Add(this.txtPP);
+            this.Controls.Add(this.txtPN);
+            this.Controls.Add(this.txtPI);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnSave);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.GridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnSA;
+        private System.Windows.Forms.DataGridView GridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblCN;
+        private System.Windows.Forms.Label lblPP;
+        private System.Windows.Forms.Label lblPN;
+        private System.Windows.Forms.Label lblPI;
+        private System.Windows.Forms.TextBox txtCN;
+        private System.Windows.Forms.TextBox txtPP;
+        private System.Windows.Forms.TextBox txtPN;
+        private System.Windows.Forms.TextBox txtPI;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnSave;
+    }
+}
+
---
+++ SkillMineCodes/ADO.NET/disConnected/Product/Form1.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+
+
+namespace ProductDisconnect
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection con;
+        SqlDataAdapter adapter;
+        SqlCommandBuilder sqlCommandBuilder;
+        DataSet ds;
+
+        public Form1()
+        {
+            InitializeComponent();
+            con = new SqlConnection(ConfigurationManager.ConnectionStrings["dbCon"].ConnectionString);
+        }
+        private DataSet GetAll()
+        {
+            adapter = new SqlDataAdapter("select * from product", con);
+            adapter.MissingSchemaAction = MissingSchemaAction.AddWithKey;
+            sqlCommandBuilder = new SqlCommandBuilder(adapter);
+            ds = new DataSet();
+            adapter.Fill(ds, "product");// product is table name given to the DataSet
+            return ds;
+        }
+
+        private void ClearForm()
+        {
+            txtPI.Clear();
+            txtPN.Clear();
+            txtPP.Clear();
+            txtCN.Clear();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ds = GetAll();
+                DataRow row = ds.Tables["product"].NewRow();
+                row["name"] = txtPN.Text;
+                row["price"] = txtPP.Text;
+                row["CompanyName"] = txtCN.Text;
+                ds.Tables["product"].Rows.Add(row);
+                int result = adapter.Update(ds.Tables["product"]);
+                if (result == 1)
+                {
+                    MessageBox.Show("Record inserted..");
+                    ClearForm();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ds = GetAll();
+                DataRow row = ds.Tables["product"].Rows.Find(txtPI.Text);
+
+                if (row != null)
+                {
+                    row["name"] = txtPN.Text;
+                    row["price"] = txtPP.Text;
+                    row["CompanyName"] = txtCN.Text;
+                    int result = adapter.Update(ds.Tables["product"]);
+                    if (result == 1)
+                    {
+                        MessageBox.Show("Record updated..");
+                        ClearForm();
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Record not found..");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ds = GetAll();
+                DataRow row = ds.Tables["product"].Rows.Find(txtPI.Text);
+
+                if (row != null)
+                {
+                    txtPN.Text = row["name"].ToString();
+                    txtPP.Text = row["price"].ToString();
+                    txtCN.Text = row["CompanyName"].ToString();
+
+                }
+                else
+                {
+                    MessageBox.Show("Record not found..");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ds = GetAll();
+                DataRow row = ds.Tables["product"].Rows.Find(txtPI.Text);
+
+                if (row != null)
+                {
+                    row.Delete();
+                    int result = adapter.Update(ds.Tables["product"]);
+                    if (result == 1)
+                    {
+                        MessageBox.Show("Record deleted..");
+                        ClearForm();
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Record not found..");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void btnSA_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ds = GetAll();
+                GridView1.DataSource = ds.Tables["product"];
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            ClearForm();
+        }
+    }
+}
