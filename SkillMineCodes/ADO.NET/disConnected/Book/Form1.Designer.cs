5d7fdcfbbc1c5d6996be64174de186d60aca5a6f
NAYAN <nayanraghuvir1@gmail.com>
2023-01-24 00:14:06 +0530

    book project ado.net dis-connected

---
+++ SkillMineCodes/ADO.NET/disConnected/Book/Form1.Designer.cs
@@ -0,0 +1,309 @@
+﻿namespace bookDisconnectPro
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblG = new System.Windows.Forms.Label();
+            this.txtE = new System.Windows.Forms.TextBox();
+            this.lblA = new System.Windows.Forms.Label();
+            this.txtPB = new System.Windows.Forms.TextBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnSA = new System.Windows.Forms.Button();
+            this.GridView1 = new System.Windows.Forms.DataGridView();
+            this.lblP = new System.Windows.Forms.Label();
+            this.lblS = new System.Windows.Forms.Label();
+            this.lblN = new System.Windows.Forms.Label();
+            this.lblR = new System.Windows.Forms.Label();
+            this.txtAN = new System.Windows.Forms.TextBox();
+            this.txtP = new System.Windows.Forms.TextBox();
+            this.txtN = new System.Windows.Forms.TextBox();
+            this.txtI = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.GridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblG
+            // 
+            this.lblG.AutoSize = true;
+            this.lblG.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblG.Location = new System.Drawing.Point(244, 221);
+            this.lblG.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lblG.Name = "lblG";
+            this.lblG.Size = new System.Drawing.Size(72, 22);
+            this.lblG.TabIndex = 94;
+            this.lblG.Text = "Edition";
+            // 
+            // txtE
+            // 
+            this.txtE.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtE.Location = new System.Drawing.Point(454, 216);
+            this.txtE.Margin = new System.Windows.Forms.Padding(5, 3, 5, 3);
+            this.txtE.Name = "txtE";
+            this.txtE.Size = new System.Drawing.Size(135, 27);
+            this.txtE.TabIndex = 93;
+            // 
+            // lblA
+            // 
+            this.lblA.AutoSize = true;
+            this.lblA.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblA.Location = new System.Drawing.Point(244, 182);
+            this.lblA.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lblA.Name = "lblA";
+            this.lblA.Size = new System.Drawing.Size(109, 22);
+            this.lblA.TabIndex = 92;
+            this.lblA.Text = "Publication";
+            // 
+            // txtPB
+            // 
+            this.txtPB.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPB.Location = new System.Drawing.Point(454, 182);
+            this.txtPB.Margin = new System.Windows.Forms.Padding(5, 3, 5, 3);
+            this.txtPB.Name = "txtPB";
+            this.txtPB.Size = new System.Drawing.Size(135, 27);
+            this.txtPB.TabIndex = 91;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(131, 398);
+            this.btnClear.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(118, 35);
+            this.btnClear.TabIndex = 90;
+            this.btnClear.Text = "CLEAR";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnSA
+            // 
+            this.btnSA.Location = new System.Drawing.Point(588, 304);
+            this.btnSA.Margin = new System.Windows.Forms.Padding(5, 3, 5, 3);
+            this.btnSA.Name = "btnSA";
+            this.btnSA.Size = new System.Drawing.Size(118, 37);
+            this.btnSA.TabIndex = 89;
+            this.btnSA.Text = "SHOW ALL";
+            this.btnSA.UseVisualStyleBackColor = true;
+            this.btnSA.Click += new System.EventHandler(this.btnSA_Click);
+            // 
+            // GridView1
+            // 
+            this.GridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.GridView1.Location = new System.Drawing.Point(441, 350);
+            this.GridView1.Margin = new System.Windows.Forms.Padding(5, 3, 5, 3);
+            this.GridView1.Name = "GridView1";
+            this.GridView1.Size = new System.Drawing.Size(527, 123);
+            this.GridView1.TabIndex = 88;
+            // 
+            // lblP
+            // 
+            this.lblP.AutoSize = true;
+            this.lblP.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblP.Location = new System.Drawing.Point(244, 154);
+            this.lblP.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lblP.Name = "lblP";
+            this.lblP.Size = new System.Drawing.Size(126, 22);
+            this.lblP.TabIndex = 87;
+            this.lblP.Text = "Author Name";
+            // 
+            // lblS
+            // 
+            this.lblS.AutoSize = true;
+            this.lblS.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblS.Location = new System.Drawing.Point(244, 113);
+            this.lblS.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lblS.Name = "lblS";
+            this.lblS.Size = new System.Drawing.Size(56, 22);
+            this.lblS.TabIndex = 86;
+            this.lblS.Text = "Price";
+            // 
+            // lblN
+            // 
+            this.lblN.AutoSize = true;
+            this.lblN.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblN.Location = new System.Drawing.Point(244, 67);
+            this.lblN.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lblN.Name = "lblN";
+            this.lblN.Size = new System.Drawing.Size(112, 22);
+            this.lblN.TabIndex = 85;
+            this.lblN.Text = "Book Name";
+            // 
+            // lblR
+            // 
+            this.lblR.AutoSize = true;
+            this.lblR.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblR.Location = new System.Drawing.Point(244, 31);
+            this.lblR.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lblR.Name = "lblR";
+            this.lblR.Size = new System.Drawing.Size(77, 22);
+            this.lblR.TabIndex = 84;
+            this.lblR.Text = "Book Id";
+            // 
+            // txtAN
+            // 
+            this.txtAN.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAN.Location = new System.Drawing.Point(454, 149);
+            this.txtAN.Margin = new System.Windows.Forms.Padding(5, 3, 5, 3);
+            this.txtAN.Name = "txtAN";
+            this.txtAN.Size = new System.Drawing.Size(135, 27);
+            this.txtAN.TabIndex = 83;
+            // 
+            // txtP
+            // 
+            this.txtP.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtP.Location = new System.Drawing.Point(454, 108);
+            this.txtP.Margin = new System.Windows.Forms.Padding(5, 3, 5, 3);
+            this.txtP.Name = "txtP";
+            this.txtP.Size = new System.Drawing.Size(135, 27);
+            this.txtP.TabIndex = 82;
+            // 
+            // txtN
+            // 
+            this.txtN.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtN.Location = new System.Drawing.Point(454, 62);
+            this.txtN.Margin = new System.Windows.Forms.Padding(5, 3, 5, 3);
+            this.txtN.Name = "txtN";
+            this.txtN.Size = new System.Drawing.Size(135, 27);
+            this.txtN.TabIndex = 81;
+            // 
+            // txtI
+            // 
+            this.txtI.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtI.Location = new System.Drawing.Point(454, 28);
+            this.txtI.Margin = new System.Windows.Forms.Padding(5, 3, 5, 3);
+            this.txtI.Name = "txtI";
+            this.txtI.Size = new System.Drawing.Size(135, 27);
+            this.txtI.TabIndex = 80;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSearch.Location = new System.Drawing.Point(270, 350);
+            this.btnSearch.Margin = new System.Windows.Forms.Padding(5, 3, 5, 3);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(118, 38);
+            this.btnSearch.TabIndex = 79;
+            this.btnSearch.Text = "SEARCH";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelete.Location = new System.Drawing.Point(131, 350);
+            this.btnDelete.Margin = new System.Windows.Forms.Padding(5, 3, 5, 3);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(118, 38);
+            this.btnDelete.TabIndex = 78;
+            this.btnDelete.Text = "DELETE";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpdate.Location = new System.Drawing.Point(270, 303);
+            this.btnUpdate.Margin = new System.Windows.Forms.Padding(5, 3, 5, 3);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(118, 38);
+            this.btnUpdate.TabIndex = 77;
+            this.btnUpdate.Text = "UPDATE";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSave.Location = new System.Drawing.Point(131, 303);
+            this.btnSave.Margin = new System.Windows.Forms.Padding(5, 3, 5, 3);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(118, 38);
+            this.btnSave.TabIndex = 76;
+            this.btnSave.Text = "SAVE";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblG);
+            this.Controls.Add(this.txtE);
+            this.Controls.Add(this.lblA);
+            this.Controls.Add(this.txtPB);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnSA);
+            this.Controls.Add(this.GridView1);
+            this.Controls.Add(this.lblP);
+            this.Controls.Add(this.lblS);
+            this.Controls.Add(this.lblN);
+            this.Controls.Add(this.lblR);
+            this.Controls.Add(this.txtAN);
+            this.Controls.Add(this.txtP);
+            this.Controls.Add(this.txtN);
+            this.Controls.Add(this.txtI);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnSave);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.GridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblG;
+        private System.Windows.Forms.TextBox txtE;
+        private System.Windows.Forms.Label lblA;
+        private System.Windows.Forms.TextBox txtPB;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnSA;
+        private System.Windows.Forms.DataGridView GridView1;
+        private System.Windows.Forms.Label lblP;
+        private System.Windows.Forms.Label lblS;
+        private System.Windows.Forms.Label lblN;
+        private System.Windows.Forms.Label lblR;
+        private System.Windows.Forms.TextBox txtAN;
+        private System.Windows.Forms.TextBox txtP;
+        private System.Windows.Forms.TextBox txtN;
+        private System.Windows.Forms.TextBox txtI;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnSave;
+    }
+}
+
---
+++ SkillMineCodes/ADO.NET/disConnected/Book/Form1.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace bookDisconnectPro
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection con;
+        SqlDataAdapter adapter;
+        SqlCommandBuilder sqlCommandBuilder;
+        DataSet ds;
+
+        public Form1()
+        {
+            InitializeComponent();
+            con = new SqlConnection(ConfigurationManager.ConnectionStrings["dbCon"].ConnectionString);
+        }
+        private void ClearForm()
+        {
+            txtI.Clear();
+            txtN.Clear();
+            txtAN.Clear();
+            txtP.Clear();
+            txtE.Clear();
+            txtPB.Clear();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private DataSet GetAll()
+        {
+            adapter = new SqlDataAdapter("select * from book", con);
+            adapter.MissingSchemaAction = MissingSchemaAction.AddWithKey;
+            sqlCommandBuilder = new SqlCommandBuilder(adapter);
+            ds = new DataSet();
+            adapter.Fill(ds, "book");// product is table name given to the DataSet
+            return ds;
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ds = GetAll();
+                DataRow row = ds.Tables["book"].NewRow();
+                row["name"] = txtN.Text;
+                row["price"] = txtP.Text;
+                row["authorName"] = txtAN.Text;
+                row["publication"]=txtPB.Text;
+                row["edition"]=txtE.Text;
+
+                ds.Tables["book"].Rows.Add(row);
+                int result = adapter.Update(ds.Tables["book"]);
+                if (result == 1)
+                {
+                    MessageBox.Show("Record inserted..");
+                    ClearForm();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            
+
+
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ds = GetAll();
+                DataRow row = ds.Tables["book"].Rows.Find(txtI.Text);
+
+                if (row != null)
+                {
+                    row["name"] = txtN.Text;
+                    row["price"] = txtP.Text;
+                    row["authorName"] = txtAN.Text;
+                    row["publication"] = txtPB.Text;
+                    row["edition"] = txtE.Text;
+
+                    int result = adapter.Update(ds.Tables["book"]);
+                    if (result == 1)
+                    {
+                        MessageBox.Show("Record updated..");
+                        ClearForm();
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Record not found..");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ds = GetAll();
+                DataRow row = ds.Tables["book"].Rows.Find(txtI.Text);
+
+                if (row != null)
+                {
+                    row.Delete();
+                    int result = adapter.Update(ds.Tables["book"]);
+                    if (result == 1)
+                    {
+                        MessageBox.Show("Record deleted..");
+                        ClearForm();
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Record not found..");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ds = GetAll();
+                DataRow row = ds.Tables["book"].Rows.Find(txtI.Text);
+
+                if (row != null)
+                {
+                    txtN.Text = row["name"].ToString();
+                    txtP.Text = row["price"].ToString();
+                    txtAN.Text = row["authorName"].ToString();
+                    txtPB.Text = row["publication"].ToString();
+                    txtE.Text = row["edition"].ToString();
+                }
+                else
+                {
+                    MessageBox.Show("Record not found..");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            ClearForm();
+        }
+
+        private void btnSA_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ds = GetAll();
+                GridView1.DataSource = ds.Tables["book"];
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
