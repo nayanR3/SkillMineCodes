efc7b96819dccac9fda8e8ed85832285232e7ec1
NAYAN <nayanraghuvir1@gmail.com>
2023-01-23 09:40:16 +0530

    student project

---
+++ SkillMineCodes/ADO.NET/studentProject/studentProject/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace studentProject
+{
+    public class Student
+    {
+        public int RollNo { get; set; }
+        public string Name { get; set; }
+        public string Stream { get; set; }
+        public int Percentage { get; set; }
+        public int Age { get; set; }
+        public int Gender { get; set; }
+    }
+}
